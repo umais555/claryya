11d8ce11f36806106313d63822c5b9cbc488b9e4
umaisiqbal <malikumais777@gmail.com>
2025-03-18 23:30:08 +0500

    donne

--- app/page.js
+++ app/page.js
@@ -1,34 +1,35 @@
 "use client";
+import { Suspense } from "react";
 import EmailVerificationSection from "@/Landing/EmailVerificationSection/EmailVerificationSection";
 import LandingPage from "@/Landing/LandingPage";
 import Header from "@/layout/Header";
-import Image from "next/image";
 import { emailVerificationData } from "../data/emailVerificationData";
 import FAQSection from "@/Landing/FAQSection/FAQSection";
 import BannerSection from "@/Landing/BannerSection/BannerSection";
 import FooterSection from "@/layout/FooterSection";
-import { usePathname } from 'next/navigation';
 import LoginSignUp from "./Authentication/LoginSignUp";
 import { useSearchParams } from 'next/navigation';
-export default function Home() {
+
+// Separate Component for SearchParams Logic
+function AuthComponent() {
   const searchParams = useSearchParams();
   const auth = searchParams.get("auth");
-  return (
-   
-      <>
-        {auth ? (
-          <LoginSignUp />
-        ) : (
-          <>
-            <Header />
-            <LandingPage />
-            <EmailVerificationSection sections={emailVerificationData} />
-            <FAQSection />
-            <FooterSection />
-          </>
-        )}
-      </>
-    );
 
-  
+  return auth ? <LoginSignUp /> : (
+    <>
+      <Header />
+      <LandingPage />
+      <EmailVerificationSection sections={emailVerificationData} />
+      <FAQSection />
+      <FooterSection />
+    </>
+  );
+}
+
+export default function Home() {
+  return (
+    <Suspense fallback={<div>Loading...</div>}>
+      <AuthComponent />
+    </Suspense>
+  );
 }
